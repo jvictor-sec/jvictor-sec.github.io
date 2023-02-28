d3e7a0d78f24ed485444cb79239023ef90eb5d6a
jvictort <jvteodorobrasil@gmail.com>
2023-02-27 22:11:47 -0300

    Lógica do scroll e menu hamburguer

--- js/main.js
+++ js/main.js
@@ -1,49 +1,24 @@
-const menuLinks = document.querySelectorAll('.menu-container li a[href^="#"]');
-const menuHeight = document.querySelector('nav').clientHeight;
-const menu = document.querySelector('.menu-container');
+const headerContainer = document.querySelector('.header-container');
 const hamburguerMenu = document.querySelector('.hamburguer-menu');
-const copyrightYear = document.querySelector('#copyright-year');
+const menuLinks = document.querySelectorAll('.nav-container li a[href^="#"]');
+const menuContainer = document.querySelector('.menu-container');
 
-// Setando o ano atual
-const date = new Date();
-copyrightYear.textContent = date.getFullYear();
-
+window.addEventListener('scroll', () => {
+  if (window.scrollY > headerContainer.offsetHeight) {
+      headerContainer.classList.add("active")
+  } else {
+      headerContainer.classList.remove("active")
+  }
+});
 
 hamburguerMenu.addEventListener('click', () => {
   hamburguerMenu.classList.toggle('active');
-  menu.classList.toggle('active');
+  menuContainer.classList.toggle('active');
 });
-
-function getDistanceFromTheTop(element) {
-  hrefSection = element.getAttribute('href');
-
-  if(hrefSection == "#skills-section") {
-    return document.querySelector('.card-container').offsetTop;
-  } else {
-    const id = hrefSection;
-    return document.querySelector(id).offsetTop;
-  }
-
-}
-
-function nativeScroll(distanceFromTheTop) {
-  window.scroll({
-    top: distanceFromTheTop,
-    behavior: 'smooth'
-  })
-}
-
-function scrollToSection(event) {
-  event.preventDefault();
-  const distanceFromTheTop = getDistanceFromTheTop(event.target) - (menuHeight);
-  console.log(menuHeight);
-  nativeScroll(distanceFromTheTop);
-}
 
 menuLinks.forEach(links => {
   links.addEventListener("click", () => {
     hamburguerMenu.classList.remove('active');
     menu.classList.remove('active');
-    scrollToSection(event);
   });
 });
